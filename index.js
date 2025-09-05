959d79607de07f5413062bbae2c84e91a4360a3a
Zion <amazion24@outlook.com>
2025-09-05 12:02:50 +0300

    הפרדנו את הראוטר מהקובץ הראשי

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3500;
+app.use(express.static(path.join(__dirname,'public')));
 
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname+"/index.html")
-})
 
+//Routers
+app.get('/',(req,res)=>{res.sendFile(__dirname+"/index.html")})
+
+const products = require('./routs/products')
+app.use('/products',products);
+
+
+
+//Start
 app.listen(port,()=>{console.log(`http://localhost:${port}`);})
